7153347cec9595cd676d9a6e9ca7aa422bf13b6c
HeekoGoesMad <adriennelie190505@gmail.com>
2025-11-16 21:51:38 +0800

    adding home page not functionally(header and navbar progress)

---
+++ src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from "./header/header";
+import { Navbar } from "./navbar/navbar";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Header, Navbar],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('Adri');
+}
