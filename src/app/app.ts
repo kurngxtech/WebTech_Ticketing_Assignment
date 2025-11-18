5cd7748a8c88d20c83ef72f7bbf9e9709b9fb06c
kurngx <kurngxtech@gmail.com>
2025-11-18 11:59:15 +0800

    Adding Photos and Admin Login Page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginPage } from "./pages/login-page/login-page";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginPage],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('web-ass-ticket');
