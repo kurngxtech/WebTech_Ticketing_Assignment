1f89d2fa7c9412d154a33809f164a1ee6d81558d
kurngx <kurngxtech@gmail.com>
2025-11-20 22:51:07 +0800

    Adding user login page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginPage } from "./pages/login-page/login-page";
+import { LoginPage } from './pages/sign-in-page-admin/login-page';
+import { UserLoginPage } from './pages/sign-in-page-user/user-login-page';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginPage],
+  imports: [
+    RouterOutlet,
+    UserLoginPage,
+    // LoginPage,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
