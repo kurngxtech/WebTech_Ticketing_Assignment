922aebeff84c4aec561413d538264e57075af882
kurngx <kurngxtech@gmail.com>
2025-11-22 21:22:12 +0800

    coba merge project pertama kali

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,16 +5,16 @@ import { UserLoginPage } from './login/sign-in-page-user/user-login-page';
 import { UserSignUp } from './login/sign-up-page-user/user-sign-up';
 
 @Component({
-  selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    // UserLoginPage,
-    // LoginPage,
-    UserSignUp,
-  ],
-  templateUrl: './app.html',
-  styleUrl: './app.css',
+   selector: 'app-root',
+   imports: [
+      RouterOutlet,
+      // UserLoginPage,
+      // LoginPage,
+      UserSignUp,
+   ],
+   templateUrl: './app.html',
+   styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('web-ass-ticket');
+   protected readonly title = signal('web-ass-ticket');
 }
