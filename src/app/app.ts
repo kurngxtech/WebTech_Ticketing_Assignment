6f37d878461283e05c3f9ef26303174bde35e760
kurngx <kurngxtech@gmail.com>
2025-11-23 15:19:38 +0800

    berhasil merge, perubahan styling login tidak terbarui

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,34 +1,25 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-<<<<<<< HEAD
 import { LoginPage } from './login/sign-in-page-admin/login-page';
 import { UserLoginPage } from './login/sign-in-page-user/user-login-page';
 import { UserSignUp } from './login/sign-up-page-user/user-sign-up';
+import { Header } from "./header/header";
+import { Footer } from "./footer/footer";
 
 @Component({
    selector: 'app-root',
    imports: [
       RouterOutlet,
-      // UserLoginPage,
-      // LoginPage,
-      UserSignUp,
+      UserLoginPage,
+      // UserSignUp,
+      //  Header,
+      //  Footer,
+      // LoginPage
    ],
    templateUrl: './app.html',
    styleUrl: './app.css',
 })
 export class App {
    protected readonly title = signal('web-ass-ticket');
-=======
-import { Header } from "./header/header";
-import { Footer } from "./footer/footer";
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
-})
-export class App {
-  protected readonly title = signal('Adri');
->>>>>>> origin/adri/pages/home
+   protected readonly titleAdri = signal('Adri');
 }
--- src/app/footer/footer.ts
+++ src/app/footer/footer.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
-  imports: [],
-  templateUrl: './footer.html',
-  styleUrl: './footer.css',
+   selector: 'app-footer',
+   imports: [],
+   templateUrl: './footer.html',
+   styleUrl: './footer.css',
 })
 export class Footer {
 
