a4e221da99fc33c4c9a83325aef3bb3aa246dc1d
kurngx <kurngxtech@gmail.com>
2025-11-23 16:27:21 +0800

    merge home page dan design login page part 2

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,21 +5,23 @@ import { UserLoginPage } from './login/sign-in-page-user/user-login-page';
 import { UserSignUp } from './login/sign-up-page-user/user-sign-up';
 import { Header } from "./header/header";
 import { Footer } from "./footer/footer";
+import { EoLoginPage } from "./login/eo-login-page/eo-login-page";
 
 @Component({
    selector: 'app-root',
    imports: [
       RouterOutlet,
-      UserLoginPage,
-      // UserSignUp,
-      //  Header,
-      //  Footer,
-      // LoginPage
+      UserSignUp
+      //  UserLoginPage,
+      // LoginPage,
+      // EoLoginPage,
+      // Header,
+      // Footer
    ],
    templateUrl: './app.html',
    styleUrl: './app.css',
 })
 export class App {
    protected readonly title = signal('web-ass-ticket');
-   protected readonly titleAdri = signal('Adri');
+   // protected readonly titleAdri = signal('Adri');
 }
